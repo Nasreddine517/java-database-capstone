1acfda684d679a89d6977166adf8e6267762dccf
Nasreddine517 <nasreddineelmekkaoui@gmail.com>
2026-09-15 09:36:28 +0200

    Create Doctor.java

---
+++ app/src/main/java/com/project/back_end/models/Doctor.java
@@ -0,0 +1,89 @@
+package com.project.back_end.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String specialty;
+
+    private String phone;
+
+    @ElementCollection
+    private List<String> availableTimes;
+
+    public Doctor() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<String> getAvailableTimes() {
+        return availableTimes;
+    }
+
+    public void setAvailableTimes(List<String> availableTimes) {
+        this.availableTimes = availableTimes;
+    }
+}
